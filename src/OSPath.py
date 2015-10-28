60ec83a819c1ca1eee18285fdbb97632cb879944
taoqsun <taoqsun@cisco.com>
2015-10-28 13:56:33 +0800

    new

---
+++ src/OSPath.py
@@ -0,0 +1,7 @@
+'''
+Created on Oct 28, 2015
+
+@author: sky
+'''
+import os
+print os.path.dirname("https://t1wdta03.qa.webex.com/t1wdta03")
---
+++ src/clearPackage.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 28, 2015
+
+@author: sky
+'''
+import subprocess
+from subprocess import Popen
+def clearWebexPackage(self):
+        """clear webex package
+        @return: operation result
+        @rtype: boolean
+        """
+        result=False
+        subprocess.Popen('rd /s /q C:\ProgramData\Webex,C:\Users\Administrator\AppData\LocalLow\Webex,C:\Users\Administrator\AppData\Local\Webex,C:\Users\Administrator\AppData\Local\Webex',shell=True)
+        if Popen.wait() == 0:
+            result=True
