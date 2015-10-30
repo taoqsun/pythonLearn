9711f6a5ac73cdfe5d46238dffc59aebe2c4f8fc
taoqsun <taoqsun@csico.com>
2015-10-30 09:33:25 +0800

    add

--- src/clearPackage.py
+++ src/clearPackage.py
@@ -4,16 +4,20 @@ Created on Oct 28, 2015
 @author: sky
 '''
 import subprocess
-from subprocess import Popen
-def clearWebexPackage(self):
-        """clear webex package
-        @return: operation result
-        @rtype: boolean
-        """
-        result=False
-#         subprocess.Popen('rd /s /q C:\ProgramData\Webex,C:\Users\Administrator\AppData\LocalLow\Webex,C:\Users\Administrator\AppData\Local\Webex,C:\Users\Administrator\AppData\Local\Webex',shell=True)
-        subprocess.Popen('rd /s /q C:\Users\Administrator\AppData\Local\Webex',shell=True)
-        subprocess.Popen('rd /s /q C:\ProgramData\Webex,C:\Users\Administrator\AppData\LocalLow\Webex',shell=True) 
-        subprocess.Popen('rd /s /q C:\ProgramData\Webex,',shell=True)
-        if Popen.wait() == 0:
-            result=True
+# from subprocess import Popen
+# def clearWebexPackage(self):
+#         """clear webex package
+#         @return: operation result
+#         @rtype: boolean
+#         """
+#         result=False
+# #         subprocess.Popen('rd /s /q C:\ProgramData\Webex,C:\Users\Administrator\AppData\LocalLow\Webex,C:\Users\Administrator\AppData\Local\Webex,C:\Users\Administrator\AppData\Local\Webex',shell=True)
+#         subprocess.Popen('rd /s /q C:\Users\Administrator\AppData\Local\Webex',shell=True)
+#         subprocess.Popen('rd /s /q C:\ProgramData\Webex,C:\Users\Administrator\AppData\LocalLow\Webex',shell=True) 
+#         subprocess.Popen('rd /s /q C:\ProgramData\Webex,',shell=True)
+#         if Popen.wait() == 0:
+#             result=True
+
+
+# subprocess.Popen('rd /s /q \"C:\Windows\Downloaded Program Files\"',shell=True)
+subprocess.Popen('rmdir /s /q C:\\500',shell=True)
