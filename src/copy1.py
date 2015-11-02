59f674eaa800f1f47450151d6ac324614d1d7fbd
taoqsun <taoqsun@cisco.com>
2015-11-02 17:02:45 +0800

    sssdd

---
+++ src/copy1.py
@@ -0,0 +1,16 @@
+﻿inputFile = open("inputFile.txt", "r")
+print ("Name of the input file: ", inputFile.name);
+
+outputFile = open("outputFile.txt", "a");
+print ("Name of the output file: ", outputFile.name);
+
+allLines = inputFile.readlines();
+for eachLine in allLines:
+    eachLine.replace("测试环境", "zjl");
+    print ("current line content: %s" % (eachLine));
+    
+     
+    #append into output file
+    outputFile.write(eachLine);
+outputFile.close();
+inputFile.close();    
