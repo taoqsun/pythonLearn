f5f5bdcd1f58bf39bc98a1649359b5004287b6f8
Administrator <Administrator@PC-201401162156>
2016-03-27 15:50:44 +0800

    modify the structure of the code

--- src/renameCurrrentDir.py
+++ src/renameCurrrentDir.py
@@ -14,10 +14,7 @@ def renameDir(oldDir="",dirValue=""):
               time.strftime('%Y-%m-%d',time.localtime(time.time())) + ".log"
     file_handle = open(logfile,"a+")
     for item in fileList:
-        
         if os.path.isfile(dirValue+os.sep+item) and item[item.find('.'):] not in [".py",".pyc" ,".log"]:
-            
-            
             if oldDir != "":
                 file_handle.writelines(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())) +
                                         " -- " + oldDir + '\\' + str(item) +"====>"+ str(flagItem) + "\n")
@@ -31,14 +28,12 @@ def renameDir(oldDir="",dirValue=""):
                 file_handle.writelines(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+
                                         " -- " + str(e) + "\n")
             flagItem = flagItem + random.randint(11,999)
-            
         if os.path.isdir(dirValue+os.sep+item):
             newDir = dirValue+os.sep+str(flagItem)+str(random.randint(11,999))
             file_handle.writelines(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+
                                         " -- " + dirValue + os.sep +item + '===>'+newDir+"\n")
             os.rename(dirValue+os.sep+item,newDir)
             renameDir(dirValue+os.sep+item,newDir)
-
     file_handle.close()
 def renameDirFromLog(dirValue=""):
     fileList = os.listdir(dirValue)
@@ -52,7 +47,6 @@ def renameDirFromLog(dirValue=""):
     #         os.c(item, flagItem)
             flagItem = flagItem + 1
             print "file ==> ",dirValue+os.sep+item
-            
         if os.path.isdir(item):
             print "dir ==> ",dirValue+os.sep+item
             print os.path.abspath(os.path.dirname(item))+os.sep+item
