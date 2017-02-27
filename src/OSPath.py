c32bb82d2cb8298f941a478fd5d3dde400cd3298
taoqsun <taoqsun@csico.com>
2017-02-27 09:50:30 +0800

    add some demos

--- src/OSPath.py
+++ src/OSPath.py
@@ -4,4 +4,5 @@ Created on Oct 28, 2015
 @author: sky
 '''
 import os
+print os.path.dirname("c:\Down\dd")
 print os.path.dirname("https://t1wdta03.qa.webex.com/t1wdta03")
--- src/iii.py
+++ src/iii.py
@@ -6,3 +6,12 @@ Created on Sep 29, 2016
 import os
 print os.path.abspath("C:\Users\Administrator\git\pythonLearn\base")
 print "jjjj".replace('old', 'new')
+nTimes = 0
+i = 0
+while nTimes < 10:
+    for i in [2,3,4]:
+        print i
+        if i == 3 :
+            break
+    print "nTimes = ",nTimes
+    nTimes += 1
