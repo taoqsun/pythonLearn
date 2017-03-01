3581fd0420af038cbfe7db125240b9a71c4322b3
taoqsun <taoqsun@TAOQSUN-WS.cisco.com>
2017-03-01 17:29:24 +0800

    commit new codes

--- src/timeLearn.py
+++ src/timeLearn.py
@@ -11,17 +11,35 @@ from datetime import datetime
 TIME_FORMAT_DEFAULT = '%Y-%m-%d %H:%M:%S'
 
 # monthCurr =int( time.strftime('%m',time.localtime(time.time())))-1
-print time.localtime()
-print time.time()
-print datetime.now()
+# print time.localtime()
+# print time.time()
+# print datetime.now()
+staticTime = datetime.now()
+print staticTime
 # print monthCurr
 # print monthCurr2
-CurrentTime = time.strftime(TIME_FORMAT_DEFAULT,time.localtime(time.time()))
-CurrentTime = datetime.strptime(CurrentTime,TIME_FORMAT_DEFAULT)
-time.sleep(1)
-job_run_time = datetime.strptime(time.strftime(TIME_FORMAT_DEFAULT,time.localtime(time.time())),TIME_FORMAT_DEFAULT)
-print CurrentTime,job_run_time
+# CurrentTime = time.strftime(TIME_FORMAT_DEFAULT,time.localtime(time.time()))
+# CurrentTime = datetime.strptime(CurrentTime,TIME_FORMAT_DEFAULT)
+# time.sleep(1)
+job_run_time = datetime.strptime("2017-02-26 11:16:06",TIME_FORMAT_DEFAULT)
+job_run_time2 = datetime.strptime("2017-02-28 11:16:07",TIME_FORMAT_DEFAULT)
+print job_run_time,job_run_time2 
+# print CurrentTime,job_run_time
 # job_run_time = time.strftime(TIME_FORMAT_DEFAULT,time.localtime(time.time()))
 # print "job_run_time =  %s ,CurrentTime =  %s"%(job_run_time,CurrentTime)
 
-print (job_run_time - CurrentTime).seconds
+# print (job_run_time - CurrentTime).seconds
+# time.sleep(1)
+print (job_run_time - staticTime).seconds
+
+print (job_run_time2 - job_run_time).seconds
+
+
+start_time = time.time()
+print start_time
+# your code
+time.sleep(0)
+elapsed_time = time.time() - start_time
+print elapsed_time
+if time.time() - start_time >= 1:
+    print "elapsed_time = ",elapsed_time
